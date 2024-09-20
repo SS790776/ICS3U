17e602da6dc4f05e68e0306828b656656a7cd431
SS790776 <790776@pdsb.net>
2024-09-20 09:16:07 -0400

    Create Lesson 4.py

---
+++ Lesson 4.py
@@ -0,0 +1,4 @@
+num = int(input("Please input a whole number: ")) 
+divider = int(input("Please input any whole number except zero"))
+answer = num/divider
+print("When", num, " is divided by 6", "the result is:", answer)
