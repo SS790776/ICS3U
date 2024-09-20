999d5fc6f4221782b775114e155a0013dd6fb0fa
SS790776 <790776@pdsb.net>
2024-09-20 09:11:31 -0400

    Create Lesson 3.py

---
+++ Lesson 3.py
@@ -0,0 +1,14 @@
+#Task A:
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute", 12)
+print(" cookies for each bowl.")
+
+#Task B:
+bowls = 6
+cookies = 144
+print("Sanjay has %d bowls for %d cookies." %(bowls, cookies))
+
+#Task C:
+cpb = cookies/bowls
+
+print("The number of cookies per bowl is: %d" %(cpb))
