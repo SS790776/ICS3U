db6c955588fb0606ba1cad41af4cf2c3812e043f
SS790776 <790776@pdsb.net>
2024-09-20 09:30:21 -0400

    Create Lesson 6.py

---
+++ Lesson 6.py
@@ -0,0 +1,8 @@
+
+birth = int(input("Please input your year of birth: "))
+age = int(input("Please input your age: "))
+calc = birth*2 + 5
+calc = calc * 50 + age
+calc = (calc - 250) /100
+
+print("This is your year of birth and your age in the same number seperated by a decimal.", calc)
