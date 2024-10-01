156d9a948935b4ab4906d9606f6648089e038521
SS790776 <790776@pdsb.net>
2024-10-01 10:52:16 -0400

    Update Unit 2, Lesson 6.py

--- Unit 2, Lesson 6.py
+++ Unit 2, Lesson 6.py
@@ -3,15 +3,21 @@ print("--------------------")
 #Count variable to make the first shape
 count = 1
 
+#let the user input the count number, to determine the size. It has to be an even number though, or else the program will crash. Hence, the while loop
 while count%2 !=0:
     count = int(input("Enter an EVEN number for the size of the shapes please: "))
 input("Ready? \n")
+#Make a second variable for the second shape
 count2 = count
+#Make 2 more variables for the third shape (will explain why later)
 temp = 0
 temp2 = 0
+#String hash and string space variables
 S = "#"
 B = " "
-for i in range(count+1, 0, -1):
+
+#Will go down 
+for i in range(count, 0, -1):
     print(S * i)
 print("--------------------")
 
