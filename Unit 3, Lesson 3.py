8b3a84f7a7984c3d5b7945268d24ef9165b4d964
SS790776 <790776@pdsb.net>
2024-10-15 09:59:36 -0400

    Update Unit 3, Lesson 3.py

--- Unit 3, Lesson 3.py
+++ Unit 3, Lesson 3.py
@@ -1,11 +1,10 @@
 items = ["Apples", "Bananas", "Cherries", "Dosa"]
 sizes = []
-for i in range(len(items)): # Predict A: State the purpose,
-                            # data types, and any output
+for i in range(len(items)): 
     sizeI = len(items[i])
     sizes.append(sizeI)
 
-for i in range(len(sizes)): # Predict B: State the output
+for i in range(len(sizes)):
     print("%d %s" % (sizes[i], items[i]))
     if sizes[i] == len(items[i]):
         print (True)
