6afc8a85e2da5097e115562d28ea86c950005aa5
SS790776 <790776@pdsb.net>
2024-10-17 10:38:08 -0400

    Update Unit 3, Lesson 8.py

--- Unit 3, Lesson 8.py
+++ Unit 3, Lesson 8.py
@@ -1,6 +1,7 @@
 
 def validate(S):
-    #
+    #False is a 2d array that stores the false letter, and the position. count is a count for each time the for loop iterates
+    #This function will go through each element in S, and if it finds a letter other than A C G or T, it will get the letter, and it's position in the string. Then, it will return True if it finds no errors, or returns the false variable if it does.
     false = []
     count = 0
     for x in S: #iterate through each element in S
