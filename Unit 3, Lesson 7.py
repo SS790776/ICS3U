ab9a484fa0546f85dcda05ae7fc1c800e9b8e1be
SS790776 <790776@pdsb.net>
2024-10-17 10:08:26 -0400

    Update Unit 3, Lesson 7.py

--- Unit 3, Lesson 7.py
+++ Unit 3, Lesson 7.py
@@ -1,15 +1,16 @@
 #Specifically, A pairs with T, and C pairs with G and vice versa.
 def comp(S):
+    #This function iterates through each element in string S. It has "new" as a variable to store the complement DNA. Once it encounters A, C, G, or T, it will add the corrosponding letter to the new variable.
     new = ''
-    for x in S:
+    for x in S: #iterate through each element in S
         if x == "A":
-            new = new + ("T")
+            new = new + ("T") #if the letter is A, add T to the new string
         elif x == "C":
-            new = new + ("G")
+            new = new + ("G")#if the letter is C, add G to the new string
         elif x == "G":
-            new = new + ("C")
+            new = new + ("C")#if the letter is G, add C to the new string
         elif x == "T":
-            new = new + ("A")
+            new = new + ("A")#if the letter is T, add A to the new string
     return new
 S = input("")
 
