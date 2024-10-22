afe29ab46abc38010d88f113147a02a6a3daf6e5
SS790776 <790776@pdsb.net>
2024-10-22 09:25:40 -0400

    Create Assignment 2, School yearbook.py

---
+++ Assignment 2, School yearbook.py
@@ -0,0 +1,39 @@
+import math as m
+
+def factors(N):
+    
+    facts = []
+    index = m.floor(m.sqrt(N))
+    for x in range(1, index+1):
+        if N % x == 0:
+            facts.append(x)
+    
+    return facts
+
+p = 1
+
+print("The School Yearbook Planner Program! ")
+print("If at any time you wish to stop the program, type 'none' \n")
+
+while p == 1:
+    N = input("Please enter the number of photographs: ")
+    
+    try:
+        N = int(N)
+        if N <= 0:
+            print(f"{N} is not a valid number, please try again \n")
+            p = 1
+        else:
+            factorsarr = factors(N)
+            index = len(factorsarr)-1
+            best = factorsarr[index]
+            ndbest = m.floor(N/best)
+            print(f"The minimum perimiter is {2*(best + ndbest)}, with dimensions {best}x{ndbest}. \n")
+            p = 1
+    except ValueError:
+        if N == "none" or N == "None" or N== "NONE":
+            print("Hope we've helped, happy yearbook planning!")
+            p = 0
+        else:
+            print(f"Please input a valid integer, I don't understand what '{N}' is \n")
+            p = 1
