47f05c4262ac7df8717510d56779f7109ee0e29b
SS790776 <790776@pdsb.net>
2024-11-18 10:52:44 -0500

    Update Unit 5, Lesson 2.py

--- Unit 5, Lesson 2.py
+++ Unit 5, Lesson 2.py
@@ -18,4 +18,18 @@ print(firstinf)
 xdim, ydim, colournum = firstinf.split(" ")
 print(xdim, ydim, colournum)
 
+"""
+# Using arrays
+colorDefs = [[0] * 2] * numColors # declare the array
+for i in range(numColors):
+   colorLine = fh.readline() # file handle must be open
+   colorLine.strip() 
+   sym, c, color = colorLine.split()
+   # you need to insert code here to exchange
+   # the tilde (~) for a space in the symbols!
+   colorDefs[i][0] = sym
+   colorDefs[i][1] = color
+
+fh.close()
+"""
 
