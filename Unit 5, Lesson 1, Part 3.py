d60bac45e232b98cbe852f342c3f67c2b8b5f019
SS790776 <790776@pdsb.net>
2024-11-19 10:03:06 -0500

    Update Unit 5, Lesson 1, Part 3.py

--- Unit 5, Lesson 1, Part 3.py
+++ Unit 5, Lesson 1, Part 3.py
@@ -3,7 +3,5 @@ import turtle
 t = turtle.Turtle()
 
 t.penup()#don't write
-t.forward(100)
-t.left(90)
-t.forward(100) #gets to 100,100
+t.goto(100,100) #goes to 100,100
 t.dot(10,'red') #makes a red dot with radius 10
