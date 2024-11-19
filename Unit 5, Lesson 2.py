be6a071d6b9f6ab150bd67f75138dfa978b6720f
SS790776 <790776@pdsb.net>
2024-11-19 10:50:18 -0500

    Update Unit 5, Lesson 2.py

--- Unit 5, Lesson 2.py
+++ Unit 5, Lesson 2.py
@@ -8,28 +8,40 @@ firstinf = []
 try:
     fh = open(file, "r")
     firstinf = fh.readline()
+    xdim, ydim, colournum = firstinf.split(" ")
+    print(xdim, ydim, colournum)
+
+    colour = {
+        
+        }
+    
+    for x in range(int(colournum)):
+        line = fh.readline()
+        line.strip()
+        symb, c, rainbow = line.split()
+        
+        if symb == "~":
+            symb = " "
+            colour.update({symb:rainbow})
+
+        else:
+            colour.update({symb:rainbow})
+    
+    print(colour)
+    
+    image = [[""]*int(xdim)]*int(ydim)
+    
+    for i in range(int(ydim)):
+        line = fh.readline()
+        for x in range(int(xdim)):
+            image[i][x] = line[x]
+    print(image)
+
     fh.close()
 except OSError as err:
     print("OSError: ", err)
 except EOFError as err2:
     print("EOFError: ", err2)
-print(firstinf)
 
-xdim, ydim, colournum = firstinf.split(" ")
-print(xdim, ydim, colournum)
 
-"""
-# Using arrays
-colorDefs = [[0] * 2] * numColors # declare the array
-for i in range(numColors):
-   colorLine = fh.readline() # file handle must be open
-   colorLine.strip() 
-   sym, c, color = colorLine.split()
-   # you need to insert code here to exchange
-   # the tilde (~) for a space in the symbols!
-   colorDefs[i][0] = sym
-   colorDefs[i][1] = color
-
-fh.close()
-"""
 
