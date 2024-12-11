89133a0aff48a5bde5288c5ccf03edf672d72a69
SS790776 <790776@pdsb.net>
2024-12-11 10:43:18 -0500

    Update Unit 5, Lesson 3.py

--- Unit 5, Lesson 3.py
+++ Unit 5, Lesson 3.py
@@ -4,10 +4,10 @@ def swap(B, p, q):
     B[q] = temp
 
 def sort(C): 
-    for i in range(len(C)):
+    for i in range(len(C)): #iterate through each element
         for j in range(i+1,len(C)-1,1): #changed so it won't swap what's already been swapped
-            if (C[i] > C[j]):
-                swap(C, i, j)
+            if (C[i] > C[j]): #if the current number is greater than the number we're on
+                swap(C, i, j) #swap the two
 
 A = [4, -1, 7, 3, 9, 0, 11, 2, 14]
 sort(A)
