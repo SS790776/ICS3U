a197f101d4217d49a1f8c4c6890790b6244e8418
SS790776 <790776@pdsb.net>
2024-12-17 10:54:48 -0500

    Update A3.py

--- A3.py
+++ A3.py
@@ -1,94 +1,264 @@
 """
 Sean S
-12/16/2024
+11/20/2024
 ICS3U 
-Programming Assignment 4
+Programming Assignment 3
 
 Variable dictionary:
-    variable - description
+    t - turtle.turtle(), makes for coding lines and using the turtle object functions a lot
+        easier.
+    file - a string that holds the name of the xpm file the code needs to use in order to
+        plot the dots.
+    fh - open(file, "r"), makes for opening files a lot easier,
+        instead of needing to code out the whole function I could just use fh.readline().
+    line - a string that holds the current line being read, will extract information from
+        this variable.
+    firstinf - an array that holds the information from the first line of the file.
+        This includes the x and y dimensions, and the total number of colours.
+    xdim - a variable split from firstinf that contains the x/horizontal dimension
+        of the xpm file that needs to be drawn.
+    ydim - a variable split from firstinf that contains the y/vertical dimension
+        of the xpm file that needs to be drawn.
+    colournum - a variable split from firstinf, holds the total number of colours
+        needed to draw the picture.
+    colour - a dictionary that holds the colours and their corresponding symbol.
+    symb - a temporary variable split from line that holds the corresponding symbol
+        of a colour.
+    c - a variable needed for unpacking and splitting colour definitions.
+    rainbow - a temporary variable split from line that holds the colour corresponding
+        to a symbol.
+    image - an array that holds all of the characters/symbols of the image,
+        each index being a seperate line.
+    err - a variable that holds the error message for OSError
+    err2 - a variable that holds the error message for EOFError
+    u - an integer that is either 1 or 0, a condition for a while loop.
+    o - an integer that is either 1 or 0, a condition for a while loop.
+    p - an integer that is either 1 or 0, a condition for a while loop.
+    ld - a string that holds the user input for background customisation.
+        L for light grey, D for dark grey, and W for white.
+    space - an integer that holds the desired spacing of the dots when drawing.
+    size - an integer that holds the desired size of the dots when drawing.
+    rotation - an integer that holds the desired rotation in degrees when drawing.
+    counter - an integer that keeps count of the amount of lines drawn. Is used
+        for calculations when drawing.
+    position - a variable that holds the turtle's calculated starting position
+        when drawing.
+    pointc - a temporary variable that holds the corresponding colour of a symbol
+        when drawing.
 """
 
-def merge(m,d,y):
+import turtle #import the turtle library
+
+
+def uprightgraph(xdim,ydim,space,size,image):
     """
-    this function takes the month, date, and year, and merges it into one number
-    :param variable: description
+    uprightgraph takes the x and y dimensions, space and size of dots, and draws the
+        image at an upright 0 degree angle.
+    :param xdim: xdim is an integer, the x dimension of the drawing so we know when to
+        stop drawing for a line.
+    :param ydim: ydim is an integer, the y dimension of the drawing so we know when the
+        last line is to stop drawing.
+    :param space: space is an integer, holds the desired spacing of the dots when drawing.
+    :param size: size is an integer, holds the desired size of the dots when drawing.
+    :param image: an array that holds all of the characters/symbols of the image,
+        each index being a seperate line.
 
-    :return: description
+    :return: draws the image based off of the xpm file and finishes the function.
     
-    variable - description.
-
-    """
-    months = {
-        "Jan": "01",
-        "Feb": "02",
-        "Mar": "03",
-        "Apr": "04",
-        "May": "05",
-        "Jun": "06",
-        "Jul": "07",
-        "Aug": "08",
-        "Sep": "09",
-        "Oct": "10",
-        "Nov": "11",
-        "Dec": "12",
-    }
-    num = y + months[m] + d
-    return int(num)
-    
-def search():
-    """
-    this function takes x, and does y, and returns z
-    :param variable: description
-
-    :return: description
-    
-    variable - description.
-
+    counter - an integer that keeps count of the amount of lines drawn. Is used
+        for calculations when drawing.
+    position - a variable that holds the turtle's calculated starting position
+        when drawing.
     """ 
     print("ploting...") #tell the user that turtle is currently plotting
     counter = 0 #create variable counter and assign to a value of 0
-def quicksort(arr):
-    """
-    quicksort
-    this function takes x, and does y, and returns z
-    :param variable: descriptioxn
-
-    :return: description
     
-    variable - description.
+    #make the turtle go to the coordinates of xdim*-space/2, ydim*space/2
+    #this coordinate places the turtle at the perfect position top left to create
+    #a centered upright drawing
+    t.goto(int(xdim)*-space/2,int(ydim)*space/2) 
+    position = t.pos() #assign the current position coordinates to variable position
+    for i in image: #iterate through each index (line) of the array image
+        counter += space #add the value of space to the counter
+        #go to the value of counter below the original starting point
+        t.goto(position[0],position[1]-counter)
+        for x in i: #iterate through each element in the current line
+            pointc = colour[x] #get the corresponding colour of the current symbol
+            t.dot(size,pointc) #create the dot with the desired size and corresponding colour
+            t.forward(space) #move forward (to the right) by the value of space
 
+def upsidedowngraph(xdim,ydim,space,size,image):
+    """
+    upsidedowngraph takes the x and y dimensions, space and size of dots, and draws the
+        image at an upside down 180 degree angle.
+    :param xdim: xdim is an integer, the x dimension of the drawing so we know when to
+        stop drawing for a line.
+    :param ydim: ydim is an integer, the y dimension of the drawing so we know when the
+        last line is to stop drawing.
+    :param space: space is an integer, holds the desired spacing of the dots when drawing.
+    :param size: size is an integer, holds the desired size of the dots when drawing.
+    :param image: an array that holds all of the characters/symbols of the image,
+        each index being a seperate line.
+
+    :return: draws the image based off of the xpm file and finishes the function.
+    
+    counter - an integer that keeps count of the amount of lines drawn. Is used
+        for calculations when drawing.
+    position - a variable that holds the turtle's calculated starting position
+        when drawing.
     """ 
-    if len(arr) <= 1:
-        return arr
-    else:
-        pivot = arr[0]
-        left = []
-        for x in arr[1:]:
-            if x < pivot:
-                left.append(x)
-        right = []
-        for i in arr[1:]:
-            if i >= pivot:
-                right.append(i)
-        
-        return quicksort(left) + [pivot] + quicksort(right)
+    print("plotting...") #tell the user that turtle is currently plotting
+    counter = 0 #create variable counter and assign to a value of 0
+    
+    #make the turtle go to the coordinates of xdim*space/2, ydim*-space/2
+    #this coordinate places the turtle at the perfect position bottom right to create
+    #a centered upside down drawing
+    t.goto(int(xdim)*space/2,int(ydim)*-space/2)
+    position = t.pos() #assign the current position coordinates to variable position
+    for i in image: #iterate through each index (line) of the array image
+        counter += space #add the value of space to the counter
+        #go to the value of counter above the original starting point
+        t.goto(position[0],position[1]+counter)
+        for x in i: #iterate through each element in the current line
+            pointc = colour[x] #get the corresponding colour of the current symbol
+            t.dot(size,pointc) #create the dot with the desired size and corresponding colour
+            t.backward(space) #move backwards (to the left) by the value of space
+def ninetygraph(xdim,ydim,space,size,image):
+    """
+    ninetygraph takes the x and y dimensions, space and size of dots, and draws the
+        image at a rotated 90 degree angle.
+    :param xdim: xdim is an integer, the x dimension of the drawing so we know when to
+        stop drawing for a line.
+    :param ydim: ydim is an integer, the y dimension of the drawing so we know when the
+        last line is to stop drawing.
+    :param space: space is an integer, holds the desired spacing of the dots when drawing.
+    :param size: size is an integer, holds the desired size of the dots when drawing.
+    :param image: an array that holds all of the characters/symbols of the image,
+        each index being a seperate line.
 
-file = 'wordle.dat'
+    :return: draws the image based off of the xpm file and finishes the function.
+    
+    counter - an integer that keeps count of the amount of lines drawn. Is used
+        for calculations when drawing.
+    position - a variable that holds the turtle's calculated starting position
+        when drawing.
+    """ 
+    print("ploting...") #tell the user that turtle is currently plotting
+    counter = 0 #create variable counter and assign to a value of 0
+    
+    #make the turtle go to the coordinates of xdim*space/2, ydim*space/2
+    #this coordinate places the turtle at the perfect position top right to create
+    #a centered 90 degree drawing
+    t.goto(int(xdim)*space/2,int(ydim)*space/2)
+    position = t.pos() #assign the current position coordinates to variable position
+    t.right(90) #turn the turtle object right by 90 degrees so it faces down
+    for i in image: #iterate through each index (line) of the array image
+        counter += space #add the value of space to the counter
+        #go to the value of counter to the left of the original starting point
+        t.goto(position[0]-counter,position[1])
+        for x in i: #iterate through each element in the current line
+            pointc = colour[x] #get the corresponding colour of the current symbol
+            t.dot(size,pointc) #create the dot with the desired size and corresponding colour
+            t.forward(space) #move forward (downwards) by the value of space
+def twoseventygraph(xdim,ydim,space,size,image):
+    """
+    twoseventygraph takes the x and y dimensions, space and size of dots, and draws the
+        image at a rotated 270 degree angle.
+    :param xdim: xdim is an integer, the x dimension of the drawing so we know when to
+        stop drawing for a line.
+    :param ydim: ydim is an integer, the y dimension of the drawing so we know when the
+        last line is to stop drawing.
+    :param space: space is an integer, holds the desired spacing of the dots when drawing.
+    :param size: size is an integer, holds the desired size of the dots when drawing.
+    :param image: an array that holds all of the characters/symbols of the image,
+        each index being a seperate line.
 
-date = [] #create the array to store the merged dates
-words = [] #create array to store the words
+    :return: draws the image based off of the xpm file and finishes the function.
+    
+    counter - an integer that keeps count of the amount of lines drawn. Is used
+        for calculations when drawing.
+    position - a variable that holds the turtle's calculated starting position
+        when drawing.
+    """ 
+    print("ploting...") #tell the user that turtle is currently plotting
+    counter = 0  #create variable counter and assign to a value of 0
+    
+    #make the turtle go to the coordinates of xdim*-space/2, ydim*-space/2
+    #this coordinate places the turtle at the perfect position bottom left to create
+    #a centered 270 degree drawing
+    t.goto(int(xdim)*-space/2,int(ydim)*-space/2)
+    position = t.pos() #assign the current position coordinates to variable position
+    t.left(90) #turn the turtle object left by 90 degrees so it faces up
+    for i in image: #iterate through each index (line) of the array image
+        counter += space #add the value of space to the counter
+        #go to the value of counter to the right of the original starting point
+        t.goto(position[0]+counter,position[1])
+        for x in i: #iterate through each element in the current line
+            pointc = colour[x] #get the corresponding colour of the current symbol
+            t.dot(size,pointc) #create the dot with the desired size and corresponding colour
+            t.forward(space) #move forward (upwards) by the value of space
+
+t = turtle.Turtle() #for easier coding while using functions in turtle
+
+#the three files used for the assignment
+#file = "smiley_emoji_mod.xpm"
+#file = "cool_smiley_mod.xpm"
+#file = "rocky_bullwinkle_mod.xpm"
+
+#ask the user for file input
+print("""What file would you like to draw?
+(1)'rocky_bullwinkle_mod.xpm' (2)'smiley_emoji_mod.xpm'
+(3)'cool_smiley_mod.xpm'  or  insert the file name of your choosing""")
+
+file = input("") #take in the input as a string
+if file == '1': #if the input is "1" then the file is rocky_bulliwnkle_mod.xpm
+    file = "rocky_bullwinkle_mod.xpm"
+elif file == '2': #if the input is "2" then the file is smiley_emoji_mod.xpm
+    file = "smiley_emoji_mod.xpm"
+elif file == '3': #if the input is "3" then the file is cool_smiley_mod.xpm
+    file = "cool_smiley_mod.xpm"
+else: 
+    #and if the input is not "1" "2" or "3" then the file name would be what was typed
+    pass
+firstinf = [] #create the array for the first line of information
 try: #make a try and except in case opening the file goes wrong
     fh = open(file, "r") #open the file
-    for i in range(1038):
-        line = fh.readline() #assign line to the next line of the file
-        line = line.strip() #clear line of "\n" characters
-        #split the line and assign into the month, day, year, and word
-        m, d, y, w = line.split()
-        words.append(w)
-        merged = merge(m,d,y)
-        date.append(merged)
-    print(words)
-    print(date)
+    firstinf = fh.readline() #assign firstinf to the first line of the file
+    #split the first line of the file and assign into the x dimension, y dimension, 
+    #and total number of colours.
+    xdim, ydim, colournum = firstinf.split(" ") 
+
+    colour = { #create the colour dictionary to store colours and corresponding symbols
+        
+        }
+    
+    for x in range(int(colournum)): #iterate for every colour
+        line = fh.readline() #read the next line
+        line.strip() #strip the leading/trailing whitespace from the text of the line
+        #split and assign the line into symb (the symbol, c, and rainbow (the colour)
+        symb, c, rainbow = line.split() 
+        
+        if symb == "~": #if the symbol is "~" then assume it is a space " "
+            symb = " "
+            #put the symbol and corresponding colour together into the dictionary
+            colour.update({symb:rainbow}) 
+
+        else: #if the symbol is not "~"
+            #put the symbol and corresponding colour together into the dictionary
+            colour.update({symb:rainbow})
+    
+    
+    
+    image = [] #create array that will store the symbols/characters of the image
+
+    for i in range(int(ydim)): #iterate through each line
+        line = fh.readline()#read the next line
+        #get rid of the "\n" endline character from what we're extracting
+        line = line.replace('\n', '') 
+        image.append(line) #add the extracted line into the image array
+
+
+
 
     fh.close()#close the file as we're done extracting information from it
 except OSError as err: #if there is an operating system error, catch it
@@ -98,18 +268,23 @@ except EOFError as err2: #if there is an error opening file error, catch it
     print("EOFError: ", err2) #print out the error
     exit() #stop the program
 
-sortedwords = quicksort(words) #sort the words alphabetically and store into sortedwords
+t.penup() #stops the turtle object from drawing lines
+t.hideturtle() #hides the turtle object from view
+turtle.tracer(0,0) #stops tracing the turtle for faster drawing
 
 u = 1 #create variable u and assign a value of 1
 while u == 1: #while u has a value of 1
-    #ask user if they want tos earch for date or words
-    dw = input("do you want to search for date or words? (D/W): ") 
+    #ask for user input of what type of background they'd like
+    ld = input("Would you like a darker or lighter background? L/D or W for no background: ") 
     
-    if ld == "D" or ld == "d": #if the user inputted a variation of "L"
-        do = "LOOK FOR DATES IG"
+    if ld == "L" or ld == "l": #if the user inputted a variation of "L"
+        turtle.bgcolor("gray70") #make the background a lighter gray
+        u = 0 #assign u to a value of 0 which exits the while loop
+    elif ld == "D" or ld == "d": #if the user inputted a variation of "D"
+        turtle.bgcolor("gray40") #make the background a darker gray
         u = 0 #assign u to a value of 0 which exits the while loop
     elif ld == "W" or ld == "w": #if the user inputted a variation of "W"
-        do = "LOOK FOR WORDS IG"
+        turtle.bgcolor("white") #make the background white
         u = 0 #assign u to a value of 0 which exits the while loop
     else: #if the user input is a response that was not expected
         print("not a valid response, please try again") #ask for user input again
@@ -156,4 +331,6 @@ while p == 1:#while p has a value of 1
         print("You did not give a valid rotation number, please try again")
         p = 1 #assign p to a value of 1 which continues the loop
     
+turtle.update() #update the turtle once it's finished drawing to see the final result
+#tell the user that turtle has finished drawing.
 print("Done! Please check to see if the Turtle graphics window has opened.")
