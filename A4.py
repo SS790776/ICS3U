3925e1514f3620e701e098b9e8b5b65b9b81ab97
SS790776 <790776@pdsb.net>
2024-12-17 23:27:30 -0500

    Update A4.py

--- A4.py
+++ A4.py
@@ -4,19 +4,41 @@ Sean S
 ICS3U 
 Programming Assignment 4
 
+
 Variable dictionary:
-    variable - description
+    arr - An array that contains the words or dates needed to be processed in functions
+    date - An array of integers representing merged dates in YYYYMMDD format.
+    words - An array of strings containing words from the Wordle archive.
+    m - A string representing the month (eg. "Jan", "Feb").
+    d - A string representing the day (eg. "01", "15").
+    y - A string representing the year (eg. "2024").
+    merged - An integer representing a merged date in YYYYMMDD format.
+    low - The starting index for binary search.
+    high - The ending index for binary search.
+    target - The value being searched for in an array.
+    size - The size of the array being sorted in exchange_sort.
+    temp - A temporary variable used for swapping words in exchange_sort.
+    temp2 - A temporary variable used for swapping dates in exchange_sort.
+    dw - User input to decide whether to search by date or word.
+    gety - User input for the year to search.
+    getm - User input for the month to search.
+    getd - User input for the day to search.
+    gotd - The merged integer date based on user input.
+    targword - A string that holds the desired word to be searched for.
+    index - The index of the target value found in the array.
+    sortedwords - An array, holding the sorted words.
+    sorteddates - An array, holding the corresponding dates.
+    u - A control variable for the program's main loop.
 """
 
 def merge(m,d,y):
     """
-    this function takes the month, date, and year, and merges it into one number
-    :param variable: description
+    This function merges the month, date, and year into one number in YYYYMMDD format.
 
-    :return: description
-    
-    variable - description.
-
+    :param m: The month as a string (eg. "Jan", "Feb").
+    :param d: The day as a string (eg. "01", "15").
+    :param y: The year as a string (eg. "2024").
+    :return: An integer representing the merged date in YYYYMMDD format.
     """
     months = {
         "Jan": "01",
@@ -38,14 +60,14 @@ def merge(m,d,y):
     
 def search(arr, low, high, target):
     """
-    this function takes x, and does y, and returns z
-    :param variable: description
+    This function uses binary search to find a target value in a sorted array.
 
-    :return: description
-    
-    variable - description.
-
-    """ 
+    :param arr: The sorted array to search, could be word or date array
+    :param low: The starting index for the search.
+    :param high: The ending index for the search.
+    :param target: The value to search for in the array.
+    :return: The index of the found target.
+    """
     if high >= low:
     
         mid = (high + low) // 2
@@ -67,14 +89,13 @@ def search(arr, low, high, target):
 
 def exchange_sort(arr,date):
     """
-    this function takes x, and does y, and returns z
-    :param variable: description
+    This function sorts an array in ascending order using the exchange sort algorithm
+    and swaps elements in the date array correspondingly.
 
-    :return: description
-    
-    variable - description.
-
-    """ 
+    :param arr: The word array to sort.
+    :param date: The date array that is swapped correspondingly
+    :return: The sorted word array and date.
+    """
     size = len(arr)
 
     for i in range(size - 1):
@@ -109,7 +130,7 @@ try: #make a try and except in case opening the file goes wrong
         m, d, y, w = line.split()
         words.append(w) #add word to words array
         merged = merge(m,d,y) #merge the month, date,and year into one number
-        date.append(merged) #
+        date.append(merged) #add the merged date into the date array
     
     fh.close()#close the file as we're done extracting information from it
 except OSError as err: #if there is an operating system error, catch it
@@ -119,39 +140,45 @@ except EOFError as err2: #if there is an error opening file error, catch it
     print("EOFError: ", err2) #print out the error
     exit() #stop the program
 
-sortedwords,sorteddates = exchange_sort(words,date) #sort the words alphabetically and store into sortedwords
+ #sort the words alphabetically and store into sortedwords,  
+ #while swapping date elements correspondingly
+sortedwords,sorteddates = exchange_sort(words,date)
 
 u = 1 #create variable u and assign a value of 1
 while u == 1: #while u has a value of 1
     #ask user if they want to search for date or words
     dw = input("do you want to search for date or words? (D/W): ") 
     
-    if dw == "D" or dw == "d": #if the user inputted a variation of "L"
-        print("What date would you like to search?")
-        gety = input("Please enter the year: ")
+    if dw == "D" or dw == "d": #if the user inputted a variation of "D"
+        print("What date would you like to search?") #Ask for desired date
+        gety = input("Please enter the year: ") #Ask for year
+        #ask for month
         getm = input("Please enter the month using the first three letters (Eg.Jan): ")
-        getd = input("Please enter the day: ")
-        gotd = merge(getm,getd,gety)
-        gotd = int(gotd)
-        if gotd > 20240421:
-            print("Our records only go until 20240421, Sorry.")
-            exit()
-        elif gotd < 20210619:
-            print("Our earliest records are 20210619, Sorry.")
-            exit()
-        index = search(sorteddates,0,len(sorteddates),gotd)
+        getd = input("Please enter the day: ") #ask for day
+        gotd = merge(getm,getd,gety) #merge into YYYYMMDD format
+        gotd = int(gotd) #turn into integer
+        if gotd > 20240421: #if the date is greater than the latest records
+            print("Our records only go until 20240421, Sorry.") #say that we don't have it
+            exit() #exit the program
+        elif gotd < 20210619: #if the date is less than the earliest records
+            print("Our earliest records are 20210619, Sorry.") #say that we don't have it
+            exit() #exit the program
+        #get the index of the target date
+        index = search(date,0,len(date),gotd) #find the index of the date
 
         
-        print(f"The word entered on {gotd} was {words[index]}.")
+        print(f"The word entered on {gotd} was {words[index]}.") #output the corresponding word
         u = 0 #assign u to a value of 0 which exits the while loop
     elif dw == "W" or dw == "w": #if the user inputted a variation of "W"
-        targword = input("Please enter the desired word: ")
-        targword = targword.upper()
-        index = search(sortedwords,0,len(sortedwords),targword)
-        if index == -1:
+        targword = input("Please enter the desired word: ") #ask for desired word
+        targword = targword.upper() #make it uppercase
+        #get index of target word
+        index = search(sortedwords,0,len(sortedwords),targword) 
+        if index == -1: #if the word doesn't exsist
+            #say that it doesn't exsist
             print("The word {targword} was not found in the database.")
-            exit()
-        print(f"The word {targword} was the solution on {date[index]}.")
+            exit() #exit the program
+        print(f"The word {targword} was the solution on {date[index]}.") #ouput the corresponding date
         u = 0 #assign u to a value of 0 which exits the while loop
     else: #if the user input is a response that was not expected
         print("not a valid response, please try again") #ask for user input again
