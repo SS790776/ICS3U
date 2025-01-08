7b98795cf504568760e8d309a8f85adc60c65940
SS790776 <790776@pdsb.net>
2025-01-08 08:58:52 -0500

    Create Unit 5, Lesson 5.py

---
+++ Unit 5, Lesson 5.py
@@ -0,0 +1,13 @@
+
+def getInt(prompt):
+    p = 0
+
+
+def fib(n):
+    return 0
+
+# DO NOT ALTER ANY CODE BELOW
+tn = getInt("Please input a whole number: ")
+for i in range(1, tn + 1):
+    print(fib(i), end = " ")
+print()
