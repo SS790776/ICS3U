be77485b1ffc2d0c9f9fce8bcd2b9b466c27db3c
SS790776 <790776@pdsb.net>
2025-01-14 08:48:52 -0500

    Update Unit 5, Lesson 5 (FLAGGED).py

--- Unit 5, Lesson 5 (FLAGGED).py
+++ Unit 5, Lesson 5 (FLAGGED).py
@@ -1,24 +1,24 @@
-def getInt(prompt):
-    p = 0
-    try:
-        response = int(input(prompt))
-        while p == 0:
-            inp = int(input("Please input a whole number greater than 0: "))
-            if inp <=0:
-                print("Please input a valid whole number")
-                p = 0
-            else:
-                p = 1
-    except ValueError as Err:
-        print("Please input a valid whole number")
-        p = 0
-        
-    return response
+
 
 def fib(n):
-    for i in range(1, n + 1):
-        print(fib(i), end = " ")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    result= (fib(n-1) + fib(n-2))
+    return result
 
-tn = getInt("Please input a whole number greater than 0: ")
-
-fib(tn)
+p = 1
+while p == 1:
+    try:
+        tn = int(input("Please input a whole number greater than 0: "))
+        if tn >0:
+            p=0
+        else:
+            p=1
+    except ValueError as Err:
+        print("ValueError:",Err)
+        p = 1
+        
+print(fib(tn))
